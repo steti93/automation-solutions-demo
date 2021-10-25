7d0a19df6146f5a1fae432201bf692e46bd08038
steslari <tislari.stefan@gmail.com>
2021-10-25 12:13:33 +0300

    Add cucumber report generation

--- src/test/java/com/automation/solutions/RunTests.java
+++ src/test/java/com/automation/solutions/RunTests.java
@@ -1,10 +1,18 @@
 package com.automation.solutions;
 
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumberOptions;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources", tags = "@Run")
+@ExtendedCucumberOptions(
+        jsonReport = "target/cucumber.json"
+)
+@CucumberOptions(
+        features = "src/test/resources",
+        tags = "not @Ignore",
+        plugin = {"json:target/cucumber.json"})
 public class RunTests {
+
 }
--- src/test/resources/features/Different-Values-Profile-Base.feature
+++ src/test/resources/features/Different-Values-Profile-Base.feature
@@ -1,5 +1,4 @@
 Feature: Sending Property name from step and use value base on profile
 
-@Run
   Scenario: Prints value that is send in step argument
     Given following property is printed env("data.users.userName")
